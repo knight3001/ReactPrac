fcf15b50ba4d9c88255996924f003b3598d9d33d
Terry <knight3001@aliyun.com>
2017-01-27 10:51:27 +1000

    Initial check in

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+}
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Clock />
+        <Clock />
+        <Clock />
+        <Clock />
+      </div>
+    );
+  }
+}
+
+export default App;
