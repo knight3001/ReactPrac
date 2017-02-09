2abc734f7d89d8c4456a435fd9007cf11479ffc0
Terry <knight3001@aliyun.com>
2017-02-09 13:36:01 +1000

    add winner effect and user name input

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../fireworks.css';
+
 const SquareNumber = 6; //min 5
 
 function Square(props) {
@@ -14,23 +16,23 @@ class Board extends Component {
     renderSquare(i) {
         let className;
         const result = this.props.finalRow;
-        if ( result[1] && result[1].indexOf(i) > -1 ){
+        if (result[1] && result[1].indexOf(i) > -1) {
             className = "square btn btn-primary";
         }
-        else{
+        else {
             className = "square btn btn-default";
         }
         className = result[0] ? className + " disabled" : className;
         return <Square className={className} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} key={i} />;
     }
 
-    renderRow(i){
+    renderRow(i) {
         let buf = [];
-        for(let j = 0; j < SquareNumber; j++){
+        for (let j = 0; j < SquareNumber; j++) {
             buf.push(this.renderSquare(j + i * SquareNumber));
         }
         return (
-            <div className="board-row" key={(i+1) * 100}>
+            <div className="board-row" key={(i + 1) * 100}>
                 {buf}
             </div>
         )
@@ -38,8 +40,8 @@ class Board extends Component {
 
     render() {
         let buf = [];
-        for (let i = 0; i < SquareNumber; i++){
-           buf.push(this.renderRow(i));
+        for (let i = 0; i < SquareNumber; i++) {
+            buf.push(this.renderRow(i));
         }
 
         return (
@@ -47,6 +49,49 @@ class Board extends Component {
                 {buf}
             </div>
         );
+    }
+}
+
+class UserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.props.onUserInput(
+            this.playerO.value,
+            this.playerX.value
+        )
+    }
+
+    render() {
+        return (
+            <form className="form-inline">
+                <div className="form-group">
+                    <label htmlFor="playerX">Player(X)</label>
+                    <input type="text" className="form-control" id="playerX" placeholder="Mike" value={this.props.playerX} ref={(input) => this.playerX = input} onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="playerO">Player(O)</label>
+                    <input type="email" className="form-control" id="playerO" placeholder="Jane" value={this.props.playerO} ref={(input) => this.playerO = input} onChange={this.handleChange} />
+                </div>
+            </form>
+        );
+    }
+}
+
+class Fireworks extends Component {
+    render() {
+        const winner = this.props.winner;
+        let className;
+        className = (winner? 'pyro' : 'hidden');
+        return (
+            <div className={className}>
+                <div className="before" />
+                <div className="after" />
+            </div>
+        )
     }
 }
 
@@ -59,8 +104,12 @@ class Game extends Component {
             }],
             xIsNext: true,
             stepNumber: 0,
-            currentMove:[]
+            currentMove: [],
+            playerX: 'Mike',
+            playerO: 'Jane'
         };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
     }
 
     handleClick(i) {
@@ -68,7 +117,7 @@ class Game extends Component {
         const current = history[this.state.stepNumber];
         const squares = current.squares.slice();
         const currentMove = this.state.currentMove;
-        
+
         if (calculateWinner(squares)[0] || squares[i]) {
             return;
         }
@@ -83,12 +132,19 @@ class Game extends Component {
         });
     }
 
+    handleUserInput(playerO, playerX) {
+        this.setState({
+            playerO: playerO,
+            playerX: playerX
+        })
+    }
+
     jumpTo(step) {
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) ? false : true,
-            history: this.state.history.slice(0,step + 1),
-            currentMove: this.state.currentMove.slice(0,step)
+            history: this.state.history.slice(0, step + 1),
+            currentMove: this.state.currentMove.slice(0, step)
         });
     }
 
@@ -96,33 +152,35 @@ class Game extends Component {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const finalRow = calculateWinner(current.squares);
-        const winner = finalRow[0];      
-        const currentMove = this.state.currentMove; 
+        const winner = finalRow[0];
+        const currentMove = this.state.currentMove;
+        const playerO = this.state.playerO;
+        const playerX = this.state.playerX;
 
         const moves = history.map((step, move) => {
-            let row = Math.floor(currentMove[move-1] / SquareNumber);
-            let line = currentMove[move-1] % SquareNumber;
+            let row = Math.floor(currentMove[move - 1] / SquareNumber);
+            let line = currentMove[move - 1] % SquareNumber;
             const desc = move ?
-                'Move #' + move + ' (Row:' + (row + 1).toString() + ' Line:' + (line + 1).toString() + ')' :
+                'Move #' + move + ' (' + (row + 1).toString() + ',' + (line + 1).toString() + ')' :
                 'Game start';
             const className = (this.state.stepNumber === move) ? 'list-group-item active' : 'list-group-item';
             return (
-                <a href="#" className={className}  key={move} onClick={() => this.jumpTo(move)}>{desc}</a>
+                <a href="#" className={className} key={move} onClick={() => this.jumpTo(move)}>{desc}</a>
             );
         });
 
         let status;
         let statusClass;
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = 'Winner: ' + (winner === 'X' ? playerX + '(X)' : playerO + '(O)');
             statusClass = "alert alert-success";
-        } 
-        else if(current.squares.indexOf(null) < 0){
+        }
+        else if (current.squares.indexOf(null) < 0) {
             status = 'Deadlock!';
             statusClass = "alert alert-warning";
         }
         else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (this.state.xIsNext ? playerX + '(X)' : playerO + '(O)');
             statusClass = "alert alert-info";
         }
 
@@ -130,9 +188,14 @@ class Game extends Component {
             <div className="panel panel-default">
                 <div className="panel-heading"><h3 className="panel-title">Tic-Tac-Toe game</h3></div>
                 <div className="panel-body">
+                    <Fireworks winner={winner} />
                     <div className="game row">
                         <div className="col-xs-12 col-sm-6 col-md-4 col-md-offset-3">
-                            <div className="input"></div>
+                            <UserForm
+                                playerX={this.state.playerX}
+                                playerO={this.state.playerO}
+                                onUserInput={this.handleUserInput}
+                                />
                             <Board
                                 finalRow={finalRow}
                                 squares={current.squares}
@@ -158,36 +221,36 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c, d] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
-            return [squares[a], [a,b,c,d]];
+            return [squares[a], [a, b, c, d]];
         }
     }
-    return [null,null];
+    return [null, null];
 }
 
-function generateWinLine(n){
+function generateWinLine(n) {
     // straight 4 to win
     let lines = [];
-    for (let i = 0; i < n; i++){
-        for(let j = 0; j < n - 3; j++){
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - 3; j++) {
             lines.push([j + i * n, j + i * n + 1, j + i * n + 2, j + i * n + 3]);
         }
     }
 
-    for (let i = 0; i < n - 3; i++){
-        for(let j = 0; j < n; j++){
-            lines.push([i * n + j , i * n + j + n, i * n + j + n*2, i * n + j + n*3]);
+    for (let i = 0; i < n - 3; i++) {
+        for (let j = 0; j < n; j++) {
+            lines.push([i * n + j, i * n + j + n, i * n + j + n * 2, i * n + j + n * 3]);
         }
     }
 
-    for (let i = 0; i < n - 3; i++){
-        for(let j = 0; j < n - 3; j++){
-            lines.push([i * n + j , i * n + j + n + 1, i * n + j + (n + 1) * 2, i * n + j + (n + 1) * 3]);
+    for (let i = 0; i < n - 3; i++) {
+        for (let j = 0; j < n - 3; j++) {
+            lines.push([i * n + j, i * n + j + n + 1, i * n + j + (n + 1) * 2, i * n + j + (n + 1) * 3]);
         }
     }
 
-    for (let i = 0; i < n - 3; i++){
-        for(let j = 3; j < n; j++){
-            lines.push([i * n + j , i * n + j + n - 1, i * n + j + (n - 1) * 2, i * n + j + (n - 1) * 3]);
+    for (let i = 0; i < n - 3; i++) {
+        for (let j = 3; j < n; j++) {
+            lines.push([i * n + j, i * n + j + n - 1, i * n + j + (n - 1) * 2, i * n + j + (n - 1) * 3]);
         }
     }
 
