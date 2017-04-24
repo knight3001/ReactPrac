ec6761b3b3896d5aeacbaacf33ea6e2c7a0487d9
Terry <knight3001@aliyun.com>
2017-04-24 15:22:05 +1000

    Real AI

--- src/game/AiMove.js
+++ src/game/AiMove.js
@@ -1,7 +1,128 @@
+const SquareNumber = 10; //min 5
+
+// squares: Array(SquareNumber * SquareNumber).fill(null)
 export function calculateAiMove(squares) {
-    for (let i = 0; i < squares.length; i++) {
-        if (squares[i] === null){
-            return i;
+    let weightO = {};
+    let weightX = {};
+    if (squares.indexOf("O") < 0) {
+        // first move for AI
+        let x = squares.indexOf("X");
+        if ((x + SquareNumber) < SquareNumber * SquareNumber) {
+            return (x + SquareNumber);
+        }
+        else {
+            return (x - SquareNumber);
+        }
+    }
+    else {
+        const lines = generateWinLine(SquareNumber);
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c, d, e] = lines[i];
+            const scores = findChance(squares, [a, b, c, d, e]);
+            if (scores["O"].length === 4 && scores["null"].length === 1) {
+                return scores["null"][0];
+            }
+            else if (scores["X"].length === 4 && scores["null"].length === 1){
+                return scores["null"][0];
+            }
+            else{
+                if (scores["O"].length === 3 && scores["null"].length === 2) {
+                    checkItemInDict(scores["null"], weightO, 10);
+                }
+                else if (scores["O"].length === 2 && scores["null"].length === 3) {
+                    checkItemInDict(scores["null"], weightO, 3);
+                }
+                else if (scores["O"].length === 1 && scores["null"].length === 4) {
+                    checkItemInDict(scores["null"], weightO, 1);
+                }
+
+                if (scores["X"].length === 3 && scores["null"].length === 2) {
+                    checkItemInDict(scores["null"], weightX, 10);
+                }
+                else if (scores["X"].length === 2 && scores["null"].length === 3) {
+                    checkItemInDict(scores["null"], weightX, 3);
+                }
+                else if (scores["X"].length === 1 && scores["null"].length === 4) {
+                    checkItemInDict(scores["null"], weightX, 1);
+                }
+            }
+        }
+        if(findMaxInDict(weightO)[0] <= findMaxInDict(weightX)[0]){
+            return findMaxInDict(weightX)[1];
+        }
+        else{
+            return findMaxInDict(weightO)[1];
         }
     }
 }
+
+export function generateWinLine(n) {
+    // straight 5 to win
+    let lines = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - 4; j++) {
+            lines.push([j + i * n, j + i * n + 1, j + i * n + 2, j + i * n + 3, j + i * n + 4]);
+        }
+    }
+
+    for (let i = 0; i < n - 4; i++) {
+        for (let j = 0; j < n; j++) {
+            lines.push([i * n + j, i * n + j + n, i * n + j + n * 2, i * n + j + n * 3, i * n + j + n * 4]);
+        }
+    }
+
+    for (let i = 0; i < n - 4; i++) {
+        for (let j = 0; j < n - 4; j++) {
+            lines.push([i * n + j, i * n + j + n + 1, i * n + j + (n + 1) * 2, i * n + j + (n + 1) * 3, i * n + j + (n + 1) * 4]);
+        }
+    }
+
+    for (let i = 0; i < n - 4; i++) {
+        for (let j = 4; j < n; j++) {
+            lines.push([i * n + j, i * n + j + n - 1, i * n + j + (n - 1) * 2, i * n + j + (n - 1) * 3, i * n + j + (n - 1) * 4]);
+        }
+    }
+
+    return lines;
+}
+
+function findChance(squares, list) {
+    let result = {"O":[], "X":[], "null": []};
+    for (let i = 0; i < list.length; i++) {
+        if (squares[list[i]] === "O") {
+            result["O"].push(list[i]);
+        }
+        else if (squares[list[i]] === "X") {
+            result["X"].push(list[i]);
+        }
+        else{
+            result["null"].push(list[i]);
+        }
+    }
+    return result;
+}
+
+function findMaxInDict(dict){
+    let maxVal = 0;
+    let index = 0;
+    for (let key in dict){
+        if(dict[key] > maxVal){
+            maxVal = dict[key];
+            index = key;
+        }
+    }
+    return [maxVal,index];
+}
+
+function checkItemInDict(list, dict, score){
+    for (let i = 0; i < list.length; i++) {
+        if(list[i] in dict){
+            dict[list[i]] += score;
+        }
+        else{
+            dict[list[i]] = score;
+        }
+    }
+}
+
+
--- src/game/Game.js
+++ src/game/Game.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { getRandomName } from './RandomName';
-import { calculateAiMove } from './AiMove';
+import { calculateAiMove, generateWinLine } from './AiMove';
 import Player from './Player';
 
 import Cryface from '../img/cryface.png';
@@ -9,7 +9,7 @@ import '../css/fireworks.css';
 import '../css/bootstrap.icon-extra.min.css';
 import '../css/bounce.css';
 
-const SquareNumber = 8; //min 5
+const SquareNumber = 10; //min 5
 
 const cursorStyle = {
     cursor: 'pointer'
@@ -376,42 +376,12 @@ function calculateWinner(squares) {
     const lines = generateWinLine(SquareNumber);
     //console.log(lines);
     for (let i = 0; i < lines.length; i++) {
-        const [a, b, c, d] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
-            return [squares[a], [a, b, c, d]];
+        const [a, b, c, d, e] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
+            return [squares[a], [a, b, c, d, e]];
         }
     }
     return [null, null];
-}
-
-function generateWinLine(n) {
-    // straight 4 to win
-    let lines = [];
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n - 3; j++) {
-            lines.push([j + i * n, j + i * n + 1, j + i * n + 2, j + i * n + 3]);
-        }
-    }
-
-    for (let i = 0; i < n - 3; i++) {
-        for (let j = 0; j < n; j++) {
-            lines.push([i * n + j, i * n + j + n, i * n + j + n * 2, i * n + j + n * 3]);
-        }
-    }
-
-    for (let i = 0; i < n - 3; i++) {
-        for (let j = 0; j < n - 3; j++) {
-            lines.push([i * n + j, i * n + j + n + 1, i * n + j + (n + 1) * 2, i * n + j + (n + 1) * 3]);
-        }
-    }
-
-    for (let i = 0; i < n - 3; i++) {
-        for (let j = 3; j < n; j++) {
-            lines.push([i * n + j, i * n + j + n - 1, i * n + j + (n - 1) * 2, i * n + j + (n - 1) * 3]);
-        }
-    }
-
-    return lines;
 }
 
 export default Game;
