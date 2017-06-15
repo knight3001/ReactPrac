22f1d45935eac86ed0c11b6ed6a3dc3c1cc8d044
Terry <knight3001@aliyun.com>
2017-06-16 09:41:21 +1000

    test redux devtools

--- src/prac/redux/index.js
+++ src/prac/redux/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { persistState } from 'redux-devtools';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import todoApp from './reducers';
-import App from './components/App';
 
-let store = createStore(todoApp)
+import App from './components/App';
+import DevTools from './containers/DevTools';
+
+const enhancer = compose(
+    applyMiddleware(thunk),
+    DevTools.instrument(),
+    persistState(
+        window.location.href.match(
+            /[?&]debug_session=([^&#]+)\b/
+        )
+    )
+);
+
+export default function configureStore(initialState) {
+    const store = createStore(todoApp, initialState, enhancer);
+
+    if (module.hot) {
+        module.hot.accept('../reducers', () =>
+            store.replaceReducer(require('../reducers').default)
+        );
+    }
+
+    return store;
+}
+
+const store = configureStore();
 
 render(
     <Provider store={store}>
