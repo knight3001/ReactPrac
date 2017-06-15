2903b48e1d5ccf1b96e334d9f98a30f9311c138c
Terry <knight3001@aliyun.com>
2017-06-15 16:29:30 +1000

    change banner, add eslint, redux devtools

--- src/App.js
+++ src/App.js
@@ -15,34 +15,35 @@ import Game from './game/Game';
 import { changeDifficult } from './game/AiMove';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cheatMode: false
-    };
-    this.modeChange = this.modeChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            cheatMode: false
+        };
+        this.modeChange = this.modeChange.bind(this);
+    }
 
-  modeChange() {
-    const cheatMode = !this.state.cheatMode;
-    this.setState({
-      cheatMode: cheatMode
-    });
-    changeDifficult();
-  }
+    modeChange() {
+        const cheatMode = !this.state.cheatMode;
+        this.setState({
+            cheatMode: cheatMode
+        });
+        changeDifficult();
+    }
 
-  render() {
-    const cheatMode = this.state.cheatMode;
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" onClick={() => this.modeChange()} />
-        </div>
-        <Game
-          cheatMode={cheatMode} />
-      </div>
-    );
-  }
+    render() {
+        const cheatMode = this.state.cheatMode;
+        return (
+            <div className="App">
+                <div className="css100">Please Refresh Your Browser</div>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" onClick={() => this.modeChange()} />
+                </div>
+                <Game
+                    cheatMode={cheatMode} />
+            </div>
+        );
+    }
 }
 
 export default App;
